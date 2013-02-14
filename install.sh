0aa2bdd65793b4cb1bd39af03c20945c07c15ac2
Goedson Paixao <goedson@gmail.com>
2013-02-14 13:16:33 -0200

    Do not install backup files
    Avoid polluting the home directory with backups (files ended in ~) of the
    config files.

--- install.sh
+++ install.sh
@@ -6,14 +6,11 @@
 
 current_dir=`pwd`
 
-for conffile in `find dotfiles -type f`
+for conffile in `find dotfiles -type f -a -not -name \*~`
 do
-    echo $current_dir/$conffile
     filepath=`echo $conffile|sed 's/^dotfiles\///'`
     dirpath=`dirname $filepath`
-#    echo $filepath
-#    echo $dirpath
     mkdir -p ~/$dirpath
+    echo installing "'$current_dir/$conffile'" to "'~/$filepath'"
     ln -sf $current_dir/$conffile ~/$filepath
-    #ln -sf $current_dir/$conffile ~/`basename $conffile`
 done
