e32c4be65c330860de660183c35f00bf9b2f8c1c
Goedson Paixao <goedson@gmail.com>
2013-05-17 10:21:21 -0300

    Added missing parameter in some client function calls

--- dotfiles/.config/awesome/rc.lua
+++ dotfiles/.config/awesome/rc.lua
@@ -4,8 +4,10 @@ require("awful.autofocus")
 require("awful.rules")
 -- Theme handling library
 require("beautiful")
+
 -- Notification library
 require("naughty")
+
 -- shifty - dynamic tagging library
 -- require("shifty")
 
@@ -282,11 +284,11 @@ globalkeys = awful.util.table.join(
 
 clientkeys = awful.util.table.join(
     awful.key({ modkey,           }, "f",      function (c) c.fullscreen = not c.fullscreen  end),
-    awful.key({ modkey, "Shift"   }, "c",      function (c) c.kill()                         end),
+    awful.key({ modkey, "Shift"   }, "c",      function (c) c.kill(c)  end),
     awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ),
-    awful.key({ modkey, "Control" }, "Return", function (c) c.swap(awful.client.getmaster()) end),
+    awful.key({ modkey, "Control" }, "Return", function (c) c.swap(c, awful.client.getmaster()) end),
     awful.key({ modkey,           }, "o",      awful.client.movetoscreen                        ),
-    awful.key({ modkey, "Shift"   }, "r",      function (c) c.redraw()                       end),
+    awful.key({ modkey, "Shift"   }, "r",      function (c) c.redraw(c)                       end),
     awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end),
     awful.key({ modkey,           }, "n",
         function (c)
