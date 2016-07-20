29e618adfeef2640cd7f13b01ca9a784d5c74567
Goedson Paixao <goedson@gmail.com>
2016-07-20 15:00:02 -0400

    Update custom variables.

--- dotfiles/.emacs-custom.el
+++ dotfiles/.emacs-custom.el
@@ -1,10 +1,18 @@
 ;; -*- mode: emacs-lisp -*-
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(hl-line ((t (:inverse-video t)))))
 
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("bc2f31d95b6336babacc96824a6993538550d6206480aa63b5368c854c3715fd" "d9046dcd38624dbe0eb84605e77d165e24fdfca3a40c3b13f504728bab0bf99d" "b7e5af48ebc8339aa39b48c6ff3f839f2c8af9b1de5f2fd5043af3fffb0352fe" "41d795001d6df08a741e40f636eecb0f949e1fd7412fe51eb475c5c34e2f14c5" "c7471ce3bb42defac344b3ecfca74228731b5ab20f804fd1deb8e65dddeab26a" default)))
+ '(magit-use-overlays nil)
  '(uniquify-buffer-name-style (quote post-forward-angle-brackets) nil (uniquify)))
